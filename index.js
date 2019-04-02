53face49d1a9784925daed1ca97f915ad33802fb
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-04-01 21:39:56 -0400

    use cassandra driver

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 const app = express();
 var cassandra = require('cassandra-driver');
-var cql = require('node-cassandra-cql');
-var client = new cql.Client({hosts: ['localhost'], keyspace: 'hw5'});
-//var client = new cassandra.Client({contactPoints: ['localhost']});
-const port = 3001;
+//var cql = require('node-cassandra-cql');
+//var client = new cql.Client({hosts: ['localhost'], keyspace: 'hw5'});
+var client = new cassandra.Client({contactPoints: ['localhost'], localDataCenter:'datacenter1', keyspace: 'hw5'});
+const port = 3005;
 
 
-//check connection to cql
+//check connection to cassandra
 client.connect(function(err, result) {
         if(err)
                 console.log(err);
@@ -23,5 +23,5 @@ app.use('/retrieve', retrieve);
 
 
 app.listen(port,'0.0.0.0', () => {
-        return console.log('App listening on port ${port}!');
+        return console.log(`App listening on port ${port}!`);
 })
